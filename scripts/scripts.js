1b3402423219d9bd12223a4efc2f6aa3f46fb2fc
vincent-valenti <vincent.valenti@bcbsnc.com>
2026-01-09 08:27:59 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -98,8 +98,8 @@ async function loadEager(doc) {
     decorateMain(main);
     document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
-    await loadCSS("https://litehouse.bcbsnc.com/cdn/v4.19.0/style.css");
-    await loadScript("https://litehouse.bcbsnc.com/cdn/v4.19.0/litehouse.js");
+    await loadCSS('https://litehouse.bcbsnc.com/cdn/v4.19.0/style.css');
+    await loadScript('https://litehouse.bcbsnc.com/cdn/v4.19.0/litehouse.js');
   }
 
   try {
